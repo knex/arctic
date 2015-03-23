fb50108314c25b8a49961599f3268f6d1d5cb2b2
Ben Drucker <bvdrucker@gmail.com>
2015-03-23 00:25:53 -0700

    Add configuration to migrator

--- test/index.js
+++ test/index.js
@@ -5,11 +5,47 @@ import Migrator from '../';
 
 describe('Migrator', () => {
 
+  let knex, migrate;
+  beforeEach(() => {
+    knex = {
+      client: {}
+    };
+    migrate = new Migrator(knex);
+  });
+
   describe('Constructor', () => {
 
     it('stores the knex instance', () => {
-      const knex = {};
-      expect(new Migrator(knex).knex).to.equal(knex);
+      expect(migrate.knex).to.equal(knex);
+    });
+
+    it('applies default configuration', () => {
+      expect(migrate.config.extension).to.equal('js');
+    });
+
+    it('applies client migration configuration', () => {
+      knex.client.migrationConfig = {
+        extension: 'ts'
+      };
+      expect(new Migrator(knex).config.extension).to.equal('ts');
+    });
+
+  });
+
+  describe('#configure', () => {
+
+    it('mutates the configuration', () => {
+      migrate.configure({extension: 'coffee'});
+      expect(migrate.config.extension).to.equal('coffee');
+    });
+
+    it('is a noop with no arguments', () => {
+      migrate.configure();
+      expect(migrate.config.extension).to.equal('js');
+    });
+
+    it('returns the migrator', () => {
+      expect(migrate.configure()).to.equal(migrate);
     });
 
   });
